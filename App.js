f8b0390676e912d62142d6dc36cae5f9b0007018
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-02-13 17:00:51 +0100

    Background and text changed to dark mode

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: '#fff' }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
