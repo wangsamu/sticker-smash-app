d2d2c11f103fba31ee173442811180c6af4e22c8
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-02-14 17:38:11 +0100

    Animations added to EmojiSticker component

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,10 +1,34 @@
 import { View, Text, Image } from 'react-native';
 import React from 'react';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 const EmojiSticker = ({ stickerSource, imageSize }) => {
+  const scaleImage = useSharedValue(imageSize);
+
+  const onDoubleTap = useAnimatedGestureHandler({
+    onActive: () => {
+      if (scaleImage.value) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    },
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
   return (
     <View style={{ top: -350 }}>
-      <Image
+      <AnimatedImage
         source={stickerSource}
         resizeMode='contain'
         style={{ width: imageSize, height: imageSize }}
